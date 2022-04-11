e845d3a898299df41c49e83ddc31ecd8e607ae07
ryukamish <10929626-ryukamish@users.noreply.github.com>
2022-04-11 17:40:09 +0530

    bash to zsh

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int borderpx = 15;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/bash";
+static char *shell = "/bin/zsh";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
 char *scroll = NULL;
